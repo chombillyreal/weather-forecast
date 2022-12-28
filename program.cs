b5ffe15acf8dd65e88fea0d0ab77db2596f02e99
chombilly <119767919+chombillyreal@users.noreply.github.com>
2022-12-28 16:26:14 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -1,35 +1,58 @@
 using System;
-using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using System.Security.Cryptography.X509Certificates;
 using System.Windows;
 using System.Xml.Linq; 
 
-  private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) 
-  {
-          try
+namespace havadurumu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public static string Meteorology(string deger)
+        {
+            string sonuc="";
+            try
             {
-                string city = Convert.ToString(comboBox1.SelectedItem);
+                string city = Convert.ToString(deger);
                 StringBuilder sb = new StringBuilder();
                 sb.Append("http://api.openweathermap.org/data/2.5/weather?q=");
                 sb.Append(city);
-                sb.Append("link continuation and add api");
+                sb.Append("&mode=xml&lang=tr&units=metric&appid=yourapi");
                 XDocument hava = XDocument.Load(sb.ToString());
                 var temp = hava.Descendants("temperature").ElementAt(0).Attribute("value").Value;
                 var city = hava.Descendants("city").ElementAt(0).Attribute("name").Value;
-                this.Text = "Test -  " + DateTime.Now.ToLongTimeString() + " Location: " + city + " Temperature: " + temp;
-
+                var cloud = hava.Descendants("clouds").ElementAt(0).Attribute("name").Value;
+                var wind = hava.Descendants("speed").ElementAt(0).Attribute("value").Value;
+                sonuc = "" + DateTime.Now.ToLongTimeString() + " Location: " + city + " Temperature: " + temp + " Clouds:" + cloud + " Wind speed: " + wind;
+                
             }
-             catch(Exception a)
+            catch (Exception g)
             {
-                MessageBox.Show(a.Message);
+                MessageBox.Show(g.Message);
             }
-            
-            
-            // The application that shows the weather of the city you choose from inside the comboBox.
-            
-            // Developed by chombilly.
-  }
+            return sonuc;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = Meteorology(comboBox1.Text);
+        }
+    }
+}
